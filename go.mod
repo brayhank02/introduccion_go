489fbe6ff6ed18aa0df265e40f1c37a111febc72
Brayhan Cardenas <brayhank.02@gmail.com>
2023-05-01 16:22:45 -0500

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/brayhank02/introduccion_go
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/brayhank02/introduccion_go/variables"
+
+func main() {
+	variables.MuestroEnteros()
+}
